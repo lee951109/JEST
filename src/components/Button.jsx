caea6fa088079ad7ae86d12c822b934b043ba2ca
lee951109 <sk985326@naver.com>
2022-05-26 15:32:41 +0900

    add lifeCycle

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 
 export default function Button () {
   const [msg, setMsg] = useState("버튼이 눌리지 않았다.");
+  const timer = useRef();
+
+  useEffect(() => {
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+    };
+  }, []);
+
+
   return (
     <div>
       <button onClick={click}>button</button>
@@ -13,7 +24,7 @@ export default function Button () {
 
   function click() {
     setMsg('버튼이 방금 눌렸다.');
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setMsg("버튼이 눌리지 않았다.");
     }, 5000);
   }
